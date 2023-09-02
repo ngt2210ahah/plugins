1a9d3523e6c6e8beaeaa4b26a2033d2eb951ee7b
ngt2210ahah <141444507+ngt2210ahah@users.noreply.github.com>
2023-09-02 19:01:32 +0700

    Update ifb.js

--- commands/general/ifb.js
+++ commands/general/ifb.js
@@ -2,14 +2,14 @@ const config = {
   name: "ifb",
   description: "info account Facebook",
   usage: ["infofb","infofacebook"],
-  credits: "Xavia Team - Tphat",
+  credits: "Xavia Team - Tphat - Duy Tuan(ndt22w) refactor // tks for Nguyen Lam (fca-zeid) for the api!",
   cooldown: 5,
 };
-
+//địt nhau liên hệ tao
 const langData = {
   vi_VN: {
     result:
-      "Thông tin của ID: {uid}\nTên người dùng: {name}\nNgày tạo tài khoản: {created_time}\nNgày sinh: {birthday}\nTình trạng mối quan hệ: {relationship_status}\nSố lượt theo dõi: {follower}\nTick xanh: {tichxanh}\nQuốc gia: {locale}, {location}",
+      "Thông tin của ID: facebook.com/{uid}\nTên người dùng: {name}\nNgày tạo tài khoản: {created_time}\nGiới tính: {gender}\nSố lượt theo dõi: {follower}\nTên đầu: {firstname}\nUsername: {username}\nTiểu sử: {tieusu}\nWebsite: {websex}\nTình trạng mối quan hệ: {relationship_status}\nĐang hẹn hò với: {lol}\nNgày sinh nhật: {birthday}\nTrích dẫn: {quotes}\nXác minh: {tichxanh}\nQuê hương: {hometown}\nNơi làm việc: {bulu}\nĐịa chỉ tài khoản: {dchi}",
     missingInput: "Vui lòng nhập ID tài khoản Facebook của bạn",
     notFound: "Không tìm thấy dữ liệu.",
     error: "Đã xảy ra lỗi. Xin lỗi vì sự bất tiện này.",
@@ -23,36 +23,75 @@ const langData = {
   },
   en_US: {
     result:
-      "ID: {uid}\nUsername: {name}\nAccount Created: {created_time}\nBirthday: {birthday}\nRelationship Status: {relationship_status}\nFollowers: {follower}\nVerified: {tichxanh}\nLocation: {locale}, {location}",
+      "ID: {uid}\nUsername: {name}\nAccount Created: {created_time}\nBirthday: {birthday}\nRelationship Status: {relationship_status}\nFollowers: {follower}\nVerified: {tichxanh2}\nLocation: {locale}, {location}",
     missingInput: "Please enter your Facebook account ID",
     notFound: "Data not found.",
     error: "An error occurred. We apologize for the inconvenience.",
   },
 };
-// API https://sumiproject.space thank!
+// API from Nguyen Lam, facebook.com/100075493308135
 async function onCall({ message, args, getLang }) {
   try {
     const input = args[0]; 
     if (!input) return message.reply(getLang("missingInput"));
     const encodedInput = encodeURIComponent(input);
-    const url = `https://sumiproject.space/facebook/getinfo?uid=${encodedInput}`;
+    const url = `https://api.zeidbot.site/fbin4v2?uid=${encodedInput}`;
     const res = await global.GET(url);
     const data = res?.data || {};
 
     if (Object.keys(data).length === 0) {
       return message.reply(getLang("notFound"));
     }
-
+    //tao them lon
+     const gender = res.data.gender ? "Nam" : "Nữ";
+    //dit con me may luon
+const tichxanh2 = res.data.tichxanh ? "Đã được xác minh" : "Không được xác minh"
     const response = getLang("result", {
+      
+      //uid
       uid: data.uid,
+      //tao thèm lồn trẻ con
+      tieusu: data.tieusu,
+      //tên đầu lồn
+      username: data.username,
+      //username đéo thích thì bỏ mẹ mày đi
+      firstname: data.firstName,
+      //tên fb
       name: data.name,
-      created_time: data.created_time,
-      birthday: data.birthday,
-      relationship_status: data.relationship_status,
+      //link fb địt mẹ mày
+      linkfb: data.link,
+      //ngày tạo acc
+      created_time: data.ngaytaoacc,
+      //số ng theo dõi
       follower: data.follower,
-      tichxanh: data.tichxanh,
-      location: data.location,
-      locale: data.locale,
+      //im momkf
+      relationship_status: data.relationship_status,
+      //happy birthday
+     birthday: data.birthday,
+     
+      //đi làm
+      work: data.work,
+      //châm ngôn thầy huấn
+      quotes: data.quotes,
+      //tick xanh
+      tichxanh: tichxanh2,
+      //que may o dau
+      hometown: res.data.hometown.name,
+      //dia chi nha may o dau
+      dchi: res.data.location.name,
+      //may lam o dau
+      bulu: res.data.work.name,
+      //crush cam sung r
+      lol: res.data.significant_other.name,
+      //sex
+      gender: gender,
+      websex: res.data.website.name,
+        //if (Object.keys(data).length === 1) {
+       //return
+                             
+      
+      
+
     });
 
     return message.reply(response);
